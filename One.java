dfc044e360000e77b464eb351cdd870daaa6bbe3
Rossi21 <rossi21@gmail.com>
2023-03-05 06:56:54 -0800

    first commit

---
+++ One.java
@@ -0,0 +1,7 @@
+public class One {
+    public static void main(String[] args){
+        System.out.println("My name is Ross Palmer");
+        System.out.println("I am 41 years old");
+        System.out.println("My home town is Seattle, Wa");
+    }
+}
